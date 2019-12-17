c846781faea66349e3ae81d452f6f6c9192cd975
olalekanarowoselu <olalekanarowoselu@yahoo.co.uk>
2019-12-17 12:09:38 +0400

    added db table prefixs databases

--- config/database.php
+++ config/database.php
@@ -59,7 +59,7 @@ return [
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => 'ebdb',
+            'prefix' => '',
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
