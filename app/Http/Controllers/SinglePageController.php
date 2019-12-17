c8623f318a440291fd42a1fab697b18d03910bf0
olalekanarowoselu <olalekanarowoselu@yahoo.co.uk>
2019-12-17 16:08:34 +0400

    remove double authentications

--- app/Http/Controllers/SinglePageController.php
+++ app/Http/Controllers/SinglePageController.php
@@ -10,7 +10,7 @@ class SinglePageController extends Controller
 
     public function __construct()
     {
-        //$this->middleware(['auth','verified'] );
+        $this->middleware(['auth','verified'] );
         //$this->middleware('verified');
     }
     public function index()
