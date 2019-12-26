cb8a3099ef2196eb6b342a6d551bcb5082ccd507
thecomputermanonline <olalekanarowoselu@gmail.com>
2019-12-26 20:27:16 +0400

    countryvisited

---
+++ database/migrations/2019_12_26_161441_create_user_visited_countries_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserVisitedCountriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_visited_countries', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_visited_countries');
+    }
+}
